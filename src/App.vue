fac0a5d66e49e7c79001dafbbc79c2038f3bcf94
sjcl <fruit@bebehp.com>
2019-06-30 18:31:27 +0900

    router

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,16 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <router-view></router-view>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Home from './components/Home'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   },
   data () {
     return {
@@ -36,3 +19,10 @@ export default {
   }
 }
 </script>
+
+<style>
+  #app {
+    background-color: #2C2F33;
+  }
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import router from './router'
 import './plugins/vuetify'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
