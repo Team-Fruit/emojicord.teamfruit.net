aa84c245ae1e8838dd77c6189b625be9c6f30f9e
sjcl <fruit@bebehp.com>
2019-07-03 13:02:43 +0900

    auth store

---
+++ src/store/modules/auth.js
@@ -0,0 +1,24 @@
+export default {
+    namespaced: true,
+    state: {
+        token: ''
+    },
+    mutations: {
+        create (state, token) {
+            state.token = token
+        },
+        destroy (state) {
+            state.token = ''
+        }
+    },
+    actions: {
+        async login({ commit }, token) {
+            commit('create', token)
+        }
+    },
+    getters: {
+        isLoggedin: state => {
+            return state.token ? true : false
+        }
+    }
+}
