f8d8e46320fff2264be20052c74e0f16c5f44895
sjcl <fruit@bebehp.com>
2019-07-03 13:03:40 +0900

    login router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import 'babel-polyfill'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import router from './router'
-import './plugins/vuetify'
-import App from './App.vue'
+import router from '@/router'
+import store from '@/store'
+import '@/plugins/vuetify'
+import App from '@/App'
 
 Vue.config.productionTip = false
 
@@ -11,5 +12,6 @@ Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App),
-  router
+  router,
+  store
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/public/Home'
+import Guilds from '@/components/private/Guilds'
+
+import Store from '@/store'
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: Home,
+      meta: {
+        isPublic: true,
+        isPublicOnly: true
+      }
+    },
+    {
+      path: '/login',
+      redirect: to => {
+        const { query } = to
+
+        if (query.token)
+          Store.dispatch('auth/login', query.token)
+
+        return { path: '/', query: null }
+      },
+      meta: {
+        isPublic: true,
+        isPublicOnly: true
+      }
+    },
+    {
+      path: '/guilds',
+      component: Guilds
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(page => page.meta.isPublicOnly) && Store.state.auth.token)
+    next('/guilds')
+  else if (to.matched.some(page => page.meta.isPublic) || Store.state.auth.token)
+    next()
+  else
+    next('/')
+})
+
+export default router
