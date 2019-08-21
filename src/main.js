d2291fd46169f5630f0049c1215c76f86b5227df
sjcl <fruit@bebehp.com>
2019-08-21 11:17:24 +0900

    update vuetify

--- src/main.js
+++ src/main.js
@@ -1,16 +1,35 @@
+// import 'babel-polyfill'
+// import Vue from 'vue'
+// import VueRouter from 'vue-router'
+// import router from '@/router'
+// import store from '@/store'
+// import '@/plugins/vuetify'
+// import App from '@/App'
+
+// Vue.config.productionTip = false
+
+// Vue.use(VueRouter)
+
+// new Vue({
+//   render: h => h(App),
+//   router,
+//   store
+// }).$mount('#app')
+
 import 'babel-polyfill'
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import App from './App.vue'
+import vuetify from './plugins/vuetify';
 import router from '@/router'
 import store from '@/store'
-import '@/plugins/vuetify'
-import App from '@/App'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
 
 new Vue({
+  vuetify,
   render: h => h(App),
   router,
   store
