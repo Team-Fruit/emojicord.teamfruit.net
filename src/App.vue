113b5f18410c87ddc9cdda39aa997a3d95b87089
sjcl <fruit@bebehp.com>
2019-09-09 18:12:11 +0900

    home header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
   <v-app>
+    <Header></Header>
     <router-view></router-view>
   </v-app>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import Header from "@/components/Header";
+import { mapGetters } from "vuex";
 
 export default {
+  components: {
+    Header
+  },
   computed: {
-    ...mapGetters('auth', {
-      isLoggedin: 'isLoggedin'
+    ...mapGetters("auth", {
+      isLoggedin: "isLoggedin"
     })
   }
-}
+};
 </script>
 
 
 <style>
-  #app {
-    background-color: #2C2F33;
-    color: #99AAB5;
-  }
+#app {
+  background-color: #2c2f33;
+  color: #99aab5;
+}
 
-  h1, h2, h3, h4, h5, h6 {
-    color: #FFFFFF;
-  }
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  color: #ffffff;
+}
 </style>
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,30 +1,19 @@
 <template>
   <v-content>
-              <Alert></Alert>
-
     <v-container fill-height>
-      <v-layout wrap align-center>
+      <v-layout align-center>
         <v-flex xs12 text-center mb-4>
-          <v-img :src="require('@/assets/logo.svg')" contain height="512"></v-img>
-        <!-- </v-flex> -->
+          <v-img :src="require('@/assets/logo.svg')" contain height="512" class="mb-3"></v-img>
+          <!-- </v-flex> -->
 
-        <!-- <v-flex text-center mb-4> -->
+          <!-- <v-flex text-center mb-4> -->
           <h1 class="display-2 font-weight-bold mb-3">Emojicord</h1>
-            <v-btn color="#7289DA" href="https://emojicord.teamfruit.net/api/auth/login">Login with Discord</v-btn>
+          <v-btn
+            color="#7289DA"
+            href="https://emojicord.teamfruit.net/api/auth/login"
+          >Login with Discord</v-btn>
         </v-flex>
       </v-layout>
     </v-container>
   </v-content>
 </template>
-
-<script>
-import Alert from "@/components/Alert";
-
-export default {
-  components: {
-    Alert
-  }
-};
-</script>
-
-
