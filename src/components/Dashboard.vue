86e0bd893e1162091fd9a8cbeb02aa631b44ccd8
sjcl <fruit@bebehp.com>
2019-09-12 14:41:40 +0900

    title tile

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,17 +2,31 @@
   <v-container grid-list-xl>
     <v-layout justify-center>
       <v-flex lg10 md11 xs12>
-        <div class="block">
+        <div class="block elevation-4">
           <v-img
             :src="require('@/assets/logo.svg')"
             contain
-            max-height="363px"
-            max-width="363px"
+            max-height="320px"
+            max-width="320px"
             class="d-none d-sm-block"
           ></v-img>
-          <div class="content fill-height pa-4">
-            <h1 class="display-2 font-weight-bold">Dashboard</h1>
-          </div>
+          <v-row>
+            <v-col>
+              <div class="content fill-height pa-4">
+                <h1 class="display-2 font-weight-bold">Emojicord</h1>
+                <p class="display-1">Discord Emojis in Minecraft</p>
+                <v-btn class="mr-2 text-none" color="#de6a3b" dark disabled>CurseForge</v-btn>
+                <v-btn
+                  class="mr-2 text-none"
+                  color="grey darken-2"
+                  dark
+                  href="https://github.com/Team-Fruit/Emojicord"
+                >
+                  <v-icon left>fab fa-github</v-icon>GitHub
+                </v-btn>
+              </div>
+            </v-col>
+          </v-row>
         </div>
       </v-flex>
     </v-layout>
@@ -22,13 +36,13 @@
 <style scoped>
 .block {
   display: flex;
-  margin-top: 110px;
+  margin-top: 120px;
   align-items: flex-end;
   background-color: #23272a;
-  height: 260px;
+  height: 220px;
 }
 
 .block > .content {
-    text-align: top;
+  text-align: top;
 }
 </style>
