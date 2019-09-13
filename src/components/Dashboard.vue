b11115fcec932d02c88d93c8ca15eccb3d25ec5b
sjcl <fruit@bebehp.com>
2019-09-13 11:43:09 +0900

    hometile

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,36 +2,74 @@
   <v-container grid-list-xl>
     <v-layout justify-center>
       <v-flex lg10 md11 xs12>
-        <div class="block elevation-4">
-          <v-img
-            :src="require('@/assets/logo.svg')"
-            contain
-            max-height="320px"
-            max-width="320px"
-            class="d-none d-sm-block"
-          ></v-img>
-          <v-row>
-            <v-col>
-              <div class="content fill-height pa-4">
-                <h1 class="display-2 font-weight-bold">Emojicord</h1>
-                <p class="display-1">Discord Emojis in Minecraft</p>
-                <v-btn class="mr-2 text-none" color="#de6a3b" dark disabled>CurseForge</v-btn>
-                <v-btn
-                  class="mr-2 text-none"
-                  color="grey darken-2"
-                  dark
-                  href="https://github.com/Team-Fruit/Emojicord"
-                >
-                  <v-icon left>fab fa-github</v-icon>GitHub
-                </v-btn>
-              </div>
-            </v-col>
-          </v-row>
-        </div>
+        <v-row>
+          <v-col>
+            <div class="block elevation-4">
+              <v-img
+                :src="require('@/assets/logo.svg')"
+                contain
+                max-height="320px"
+                max-width="320px"
+                class="d-none d-sm-block"
+              ></v-img>
+              <v-row>
+                <v-col>
+                  <div class="content fill-height pa-4">
+                    <h1 class="display-2 font-weight-bold">Emojicord</h1>
+                    <p class="display-1">Discord Emojis in Minecraft</p>
+                    <v-btn class="mr-2 text-none" color="#de6a3b" dark disabled>CurseForge</v-btn>
+                    <v-btn
+                      class="mr-2 text-none"
+                      color="grey darken-2"
+                      dark
+                      href="https://github.com/Team-Fruit/Emojicord"
+                    >
+                      <v-icon left>fab fa-github</v-icon>GitHub
+                    </v-btn>
+                  </div>
+                </v-col>
+              </v-row>
+            </div>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-flex sm4 xs12 v-for="tile in tiles" :key="tile.title">
+            <HomeTile :title="tile.title" :icon="tile.icon" :desc="tile.desc"></HomeTile>
+          </v-flex>
+        </v-row>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
+
+<script>
+import HomeTile from "@/components/home/HomeTile";
+
+export default {
+  components: {
+    HomeTile
+  },
+  data: () => ({
+    tiles: [
+      {
+        title: "Guilds",
+        icon: "fas fa-users",
+        desc: "Invite a bot and get Emoji from the guild"
+      },
+      {
+        title: "Emojis",
+        icon: "fas fa-grin-tongue-wink",
+        desc: "Choose what you want to use with Emojicord in the Emoji list"
+      },
+      {
+        title: "Download",
+        icon: "fas fa-download",
+        desc: "Generate download code and import Emoji into Minecraft"
+      }
+    ]
+  })
+};
+</script>
 
 <style scoped>
 .block {
---
+++ src/components/home/HomeTile.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card class="mx-auto fill-height" color="#23272A" dark>
+    <v-card-title>
+      <v-icon large left>{{ this.icon }}</v-icon>{{ this.title }}
+    </v-card-title>
+    <v-card-text>{{ this.desc }}</v-card-text>
+    <v-card-actions>
+      <div class="flex-grow-1"></div>
+      <v-btn color="#43b581">{{ this.title }}</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        icon: String,
+        desc: String
+    }
+}
+</script>
+
+<style scoped>
+.actions {
+    
+}
+</style>
