043eefe1a55bbb01303b3d6b499aaf74ddf09c93
sjcl <fruit@bebehp.com>
2019-10-24 22:44:40 +0900

    Google Analytics

--- src/main.js
+++ src/main.js
@@ -7,12 +7,17 @@ import store from '@/store'
 import VueRouter from 'vue-router'
 import VueClipboard from 'vue-clipboard2'
 import Notifications from 'vue-notification'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
 Vue.use(VueClipboard)
 Vue.use(Notifications)
+Vue.use(VueAnalytics, {
+  id: 'UA-150873600-1',
+  router
+})
 
 new Vue({
   vuetify,
