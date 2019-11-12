7b1fdd2ae3e01d1544001030f33130a0353fa1fe
sjcl <fruit@bebehp.com>
2019-11-12 14:18:24 +0900

    pre render

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
 module.exports = {
-    transpileDependencies: ['vuetify']
+    transpileDependencies: ['vuetify'],
+    configureWebpack: () => {
+        if (process.env.NODE_ENV === 'production') {
+          return {
+            plugins: [
+              new PrerenderSPAPlugin({
+                staticDir: path.join(__dirname, 'dist'),
+                routes: ['/']
+              })
+            ]
+          }
+        }
+      }
 }
