aa3940f14551df73d4f5a182e48a61469234ceab
sjcl <fruit@bebehp.com>
2020-09-24 06:46:35 +0900

    emoji dialog

--- src/components/newemojis/EmojiItem.vue
+++ src/components/newemojis/EmojiItem.vue
@@ -6,6 +6,7 @@
     width="80px"
     @mouseenter="onEnter()"
     @mouseleave="onLeave()"
+    @click.stop="dialog = true"
   >
     <v-img
       contain
@@ -13,22 +14,77 @@
       height="64px"
       width="64px"
     ></v-img>
+
+    <v-dialog v-model="dialog" width="unset">
+      <v-card dark>
+        <v-container>
+          <v-row>
+            <v-col cols="auto">
+              <v-img
+                contain
+                :src="`https://cdn.discordapp.com/emojis/${emoji.id}?size=64`"
+                height="64px"
+                width="64px"
+              ></v-img>
+            </v-col>
+            <v-col cols="auto" class>
+              <v-row class="flex-column fill-height" justify="center">
+                <v-col class="emojiname">:{{ this.emoji.name }}:</v-col>
+                <v-col class="py-0">
+                  <v-switch
+                    v-model="this.emoji.enabled"
+                    class="my-0"
+                    color="blue"
+                    hide-details
+                    label="Include in Minecraft"
+                    @change="onChangeInclude()"
+                  ></v-switch>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-dialog>
   </v-card>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   props: {
-    emoji: Object
+    emoji: Object,
+  },
+  data() {
+    return {
+      dialog: false,
+    };
   },
   methods: {
+    ...mapActions("http", ["put", "delete"]),
     onEnter() {
       this.$emit("onHover", this.emoji);
     },
     onLeave() {
       this.$emit("onHover", null);
-    }
-  }
+    },
+    // TODO: コンポーネントでやるのは不適切で、Vuexに持っていくべき.
+    // TODO: 親に渡していないので何もアップデートされません、上記で同時に解決出来そう.
+    onChangeInclude() {
+      if (this.emoji.enable) {
+        this.put(`/user/emojis/${this.emoji.id}`).catch((err) => {
+          this.emoji.enable = false;
+          return err;
+        });
+      } else {
+        this.delete(`/user/emojis/${this.emoji.id}`).catch((err) => {
+          this.emoji.enable = true;
+          return err;
+        });
+      }
+    },
+  },
 };
 </script>
 
@@ -39,5 +95,11 @@ export default {
 
 .emoji:hover {
   background-color: rgb(79, 84, 92);
+}
+
+.emojiname {
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  vertical-align: top;
 }
 </style>
